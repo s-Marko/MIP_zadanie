05a6c3e4189edcb4cf575b9c86b9bc49bf3d0e98
s-Marko <Samuel.Marko1@gmail.com>
2019-12-15 23:23:16 +0100

    Added files to .gitignore

--- Hospital2/src/saf/Doctor.java
+++ Hospital2/src/saf/Doctor.java
@@ -3,8 +3,7 @@ package saf;
 public class Doctor extends Person{
 
 	public void diagnose(hospital.Patient patient) {
-		System.out.println(patient.getFirstName() + " has " + patient.getDisease());
-		
+		System.out.println(patient.getFirstName() + " has " + patient.getDisease());		
 	}
 	
 	public void whatAmI() {
--- Hospital2/src/saf/Patient.java
+++ Hospital2/src/saf/Patient.java
@@ -4,9 +4,7 @@ public class Patient extends Person{
 
 	public void diagnose(hospital.Patient patient) {
 		System.out.println("I can't diagnose you...");		
-	}
-	
-	
+	}	
 	
 	public void whatAmI() {
 		System.out.println("I am Patient");
